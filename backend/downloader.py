a711a77d28dac102980c013450abb2883e1f6f7c
Raj Poddar <rajpoddar8907@gmail.com>
2026-09-19 12:33:13 +0530

    Fix youtube bot detection reload error

--- backend/downloader.py
+++ backend/downloader.py
@@ -54,10 +54,10 @@ def get_yt_dlp_options(quiet=True) -> dict:
         'ignoreerrors': False,
         'nocheckcertificate': True,
         'geo_bypass': True,
-        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
-        'http_headers': {
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'extractor_args': {
+            'youtube': {
+                'player_client': ['android', 'web']
+            }
         },
     }
 
